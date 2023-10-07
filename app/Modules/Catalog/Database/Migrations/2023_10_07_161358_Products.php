b3f0fd1fe1b5b648ae105374a35f178dd63205d3
IvanKornev <brovkin.komar@yandex.ru>
2023-10-07 19:26:54 +0300

    feat: миграция, добавляющая category_id и SEO-поля для продукта

--- app/Modules/Catalog/Database/Migrations/2023_10_07_161358_Products.php
+++ app/Modules/Catalog/Database/Migrations/2023_10_07_161358_Products.php
@@ -7,35 +7,33 @@ use Illuminate\Database\Migrations\Migration;
 return new class extends Migration
 {
     /**
-     * Создает таблицу категорий
+     * Добавляет FK-поле category_id, а также SEO-поля
      *
      * @return void
      */
     public function up(): void
     {
-        Schema::create('categories', function (Blueprint $table) {
-            $table->id();
-            $table->string('name');
-            $table->string('slug');
-            $table->foreignId('parent_id')
-                ->nullable()
+        Schema::table('products', function (Blueprint $table) {
+            $table->foreignId('category_id')
                 ->references('id')
                 ->on('categories')
                 ->onDelete('cascade');
-            $table->string('image');
             $table->string('seo_title');
             $table->text('seo_description');
-            $table->timestamps();
         });
     }
 
     /**
-     * Удаляет таблицу категорий
+     * Убирает поле category_id и SEO-поля
      *
      * @return void
      */
     public function down(): void
     {
-        Schema::dropIfExists('categories');
+        Schema::table('products', function (Blueprint $table) {
+            $table->dropColumn('category_id');
+            $table->dropColumn('seo_title');
+            $table->dropColumn('seo_description');
+        });
     }
 };
