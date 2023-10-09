5f6c4adcde4840a5be09943738d15e66edaf1acb
IvanKornev <brovkin.komar@yandex.ru>
2023-10-09 20:52:10 +0300

    build: переписал phpunit.xml, чтоб он запускал feature/unit-тесты, находящиеся в модулях

---
+++ app/Modules/Catalog/Tests/Feature/CatalogTreeTest.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Modules\Catalog\Tests\Unit;
+
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+final class CatalogTreeTest extends TestCase
+{
+    /**
+     * Проверяет получение дерева категорий
+     *
+     * @return void
+     */
+    public function test_returns_the_catalog_tree(): void
+    {
+        $this->assertTrue(true);
+    }
+}
--- tests/CreatesApplication.php
+++ tests/CreatesApplication.php
@@ -8,14 +8,12 @@ use Illuminate\Foundation\Application;
 trait CreatesApplication
 {
     /**
-     * Creates the application.
+     * Создаем инстанс приложения
      */
     public function createApplication(): Application
     {
         $app = require __DIR__.'/../bootstrap/app.php';
-
         $app->make(Kernel::class)->bootstrap();
-
         return $app;
     }
 }
