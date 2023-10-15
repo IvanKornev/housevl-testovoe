ab0b7c7aebd6e5d95373235c77e3d20964e427ee
IvanKornev <brovkin.komar@yandex.ru>
2023-10-15 12:22:32 +0300

    fix: ресурс для характеристик товара

--- app/Modules/Catalog/Transformers/ProductResource.php
+++ app/Modules/Catalog/Transformers/ProductResource.php
@@ -15,15 +15,17 @@ class ProductResource extends JsonResource
      */
     public function toArray(Request $request): array
     {
+        $characteristics = $this->whenLoaded('characteristics');
         return [
             'id' => $this->id,
             'name' => $this->name,
             'slug' => $this->slug,
             'image' => $this->image,
-            'description' => $this->image,
+            'description' => $this->description,
             'price' => $this->price,
             'createdAt' => $this->created_at,
             'updatedAt' => $this->updated_at,
+            'characteristics' => new ProductCharacteristicResource($characteristics),
         ];
     }
 }
