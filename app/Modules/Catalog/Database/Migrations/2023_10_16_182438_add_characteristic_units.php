c4666895c78ba1948589496886bac769d2f7905c
IvanKornev <brovkin.komar@yandex.ru>
2023-10-16 21:28:17 +0300

    fix: добавляю через миграцию единицы измерения для таблицы

--- app/Modules/Catalog/Database/Migrations/2023_10_16_182438_add_characteristic_units.php
+++ app/Modules/Catalog/Database/Migrations/2023_10_16_182438_add_characteristic_units.php
@@ -7,17 +7,17 @@ use Illuminate\Database\Migrations\Migration;
 return new class extends Migration
 {
     /**
-     * Обновляет таблицу характеристик через миграцию
+     * Добавляет единицы измерения для каждой характеристики
      *
      * @return void
      */
     public function up(): void
     {
         Schema::table('product_characteristics', function (Blueprint $table) {
-            $table->string('weight')->change();
-            $table->string('length')->change();
-            $table->string('width')->change();
-            $table->string('height')->change();
+            $table->string('weight_unit')->default('кг.');
+            $table->string('length_unit')->default('см.');
+            $table->string('width_unit')->default('см.');
+            $table->string('height_unit')->default('см.');
         });
     }
 
@@ -29,10 +29,10 @@ return new class extends Migration
     public function down()
     {
         Schema::table('product_characteristics', function (Blueprint $table) {
-            $table->integer('weight')->change();
-            $table->integer('length')->change();
-            $table->integer('width')->change();
-            $table->integer('height')->change();
+            $table->dropColumn('weight_unit');
+            $table->dropColumn('length_unit');
+            $table->dropColumn('width_unit');
+            $table->dropColumn('height_unit');
         });
     }
 };
