eed41c41a67c401041786b2e9a592c7478c7f470
IvanKornev <brovkin.komar@yandex.ru>
2023-10-23 19:42:12 +0300

    refactor: переиспользую атрибут характеристик в их ресурсе

--- app/Modules/Catalog/Transformers/Product/CharacteristicResource.php
+++ app/Modules/Catalog/Transformers/Product/CharacteristicResource.php
@@ -15,14 +15,6 @@ class CharacteristicResource extends JsonResource
      */
     public function toArray(Request $request): array
     {
-        $results = [];
-        $possibleFields = ['weight', 'length', 'width', 'height'];
-        foreach ($possibleFields as $field) {
-            $value = $this->resource[$field];
-            $unitField = "{$field}_unit";
-            $unit = $this->resource[$unitField];
-            $results[$field] = "$value $unit";
-        }
-        return $results;
+        return $this->resource->valuesWithUnits;
     }
 }
