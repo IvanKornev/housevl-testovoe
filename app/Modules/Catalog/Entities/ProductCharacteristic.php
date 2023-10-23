04575f71799c593d98e9ccf598f8177b4bac502c
IvanKornev <brovkin.komar@yandex.ru>
2023-10-23 19:57:09 +0300

    refactor: вынес характеристики товара в enum и прохожусь по ним

--- app/Modules/Catalog/Entities/ProductCharacteristic.php
+++ app/Modules/Catalog/Entities/ProductCharacteristic.php
@@ -8,6 +8,7 @@ use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Model;
 
 use App\Modules\Catalog\Database\Factories\ProductCharacteristicFactory;
+use App\Modules\Catalog\Enums\ProductCharacteristicEnum;
 
 class ProductCharacteristic extends Model
 {
@@ -34,9 +35,9 @@ class ProductCharacteristic extends Model
     protected function valuesWithUnits(): Attribute
     {
         $getterCallback = function (mixed $value, array $attrs): array {
-            $fields = ['weight', 'length', 'width', 'height'];
             $results = [];
-            foreach ($fields as $fieldName) {
+            foreach (ProductCharacteristicEnum::cases() as $value) {
+                $fieldName = strtolower($value->name);
                 $value = $attrs[$fieldName];
                 $unitField = "{$fieldName}_unit";
                 $unit = $attrs[$unitField];
