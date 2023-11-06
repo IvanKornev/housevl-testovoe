d7cf8302aa633fa9dd0279085ad3f1e68bf5428e
IvanKornev <brovkin.komar@yandex.ru>
2023-11-06 16:27:05 +0300

    feat: валидация запроса на регистрация

--- app/Modules/User/Http/Controllers/AuthController.php
+++ app/Modules/User/Http/Controllers/AuthController.php
@@ -2,9 +2,10 @@
 
 namespace App\Modules\User\Http\Controllers;
 
-use Illuminate\Http\Request;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Routing\Controller;
+
+use App\Modules\User\Http\Requests\RegistrationRequest;
 
 class AuthController extends Controller
 {
@@ -13,7 +14,7 @@ class AuthController extends Controller
      * Регистрирует нового пользователя
      * @return JsonResponse
      */
-    public function register(Request $request): JsonResponse
+    public function register(RegistrationRequest $request): JsonResponse
     {
         return response()->json([
             'message' => 'Пользователь успешно зарегистрирован',
---
+++ app/Modules/User/Http/Requests/RegistrationRequest.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Modules\User\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+use App\Shared\Http\Traits\HasJsonRequestError;
+
+final class RegistrationRequest extends FormRequest
+{
+    use HasJsonRequestError;
+
+    /**
+     * Валидационые правила по отношению к запросу
+     *
+     * @return array
+     */
+    public function rules(): array
+    {
+        return [
+            'name' => 'required|string|max:300',
+            'surname' => 'required|string|max:300',
+            'patronymic' => 'required|string|max:300',
+            'email' => 'required|email|max:300',
+            'phone' => 'phone',
+        ];
+    }
+
+    /**
+     *
+     * @return bool
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+}
