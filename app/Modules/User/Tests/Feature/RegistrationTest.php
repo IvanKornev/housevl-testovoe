c9ead12e20fc91af8a3366983963408b38bc55f2
IvanKornev <brovkin.komar@yandex.ru>
2023-11-06 19:09:27 +0300

    test: тест, проверяющий возврат ошибки при неуникальной почте (пока failed)

--- app/Modules/User/Tests/Feature/RegistrationTest.php
+++ app/Modules/User/Tests/Feature/RegistrationTest.php
@@ -17,19 +17,58 @@ final class RegistrationTest extends TestCase
      */
     private const URL = '/api/auth/registration';
 
+    /**
+     * Проверяет фичу регистрации
+     *
+     * @return void
+     */
     public function testRegistersUserInTheSystem(): void
     {
-        $userForm = User::factory()->make()->toArray();
+        $userForm = $this->getUserForm();
         $response = $this->json('POST', self::URL, $userForm);
         $response->assertStatus(200);
         $content = json_decode($response->getContent(), true);
         $this->assertIsArray($content['record']);
     }
 
+    /**
+     * Проверяет возврат ошибки при провале валидации
+     *
+     * @return void
+     */
     public function testFailsRequestValidation(): void
     {
         $userForm = ['name' => false];
         $response = $this->json('POST', self::URL, $userForm);
         $response->assertStatus(422);
+    }
+
+    /**
+     * Проверяет возврат ошибки при дублировании названия почты
+     *
+     * @return void
+     */
+    public function testReturnsAnErrorIfEmailIsNotUnique (): void
+    {
+        $userForm = $this->getUserForm();
+        $this->json('POST', self::URL, $userForm);
+        $response = $this->json('POST', self::URL, $userForm);
+        $response->assertStatus(500);
+        $content = json_decode($response->getContent(), true);
+        $expectedMessage = 'Пользователь с такой почтой существует';
+        $this->assertEquals($content['message'], $expectedMessage);
+    }
+
+    /**
+     * Возвращает содержимое формы пользователя
+     *
+     * @return array
+     */
+    private function getUserForm(): array
+    {
+        $results = User::factory()->make()
+            ->makeVisible(['password'])
+            ->toArray();
+        return $results;
     }
 }
