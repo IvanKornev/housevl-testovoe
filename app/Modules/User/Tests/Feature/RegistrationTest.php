ef04f2b776d35f287b4319ece32e7f1f156f89f8
IvanKornev <brovkin.komar@yandex.ru>
2023-11-06 16:07:03 +0300

    test: проверяет успешность регистрации юзера (пока failed)

---
+++ app/Modules/User/Tests/Feature/RegistrationTest.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Modules\User\Tests\Feature;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use App\Modules\User\Entities\User;
+use Tests\TestCase;
+
+final class RegistrationTest extends TestCase
+{
+    use RefreshDatabase;
+
+    /**
+     * URL вызываемого эндпоинта
+     *
+     * @var string
+     */
+    private const URL = '/api/auth/registration';
+
+    public function testRegistersUserInTheSystem(): void
+    {
+        $userForm = User::factory()->make()->toArray();
+        $response = $this->json('POST', self::URL, $userForm);
+        $response->assertStatus(200);
+    }
+}
