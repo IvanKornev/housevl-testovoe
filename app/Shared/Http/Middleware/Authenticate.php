a6a8232ac8841d10f3457f89c1712aeeb64a7141
IvanKornev <brovkin.komar@yandex.ru>
2023-11-19 12:26:10 +0300

    fix: переписал Authenticate->unauthenticated

--- app/Shared/Http/Middleware/Authenticate.php
+++ app/Shared/Http/Middleware/Authenticate.php
@@ -5,15 +5,16 @@ declare(strict_types=1);
 namespace App\Shared\Http\Middleware;
 
 use Illuminate\Auth\Middleware\Authenticate as Middleware;
-use Illuminate\Http\Request;
+use Illuminate\Auth\AuthenticationException;
 
 class Authenticate extends Middleware
 {
     /**
-     * Get the path the user should be redirected to when they are not authenticated.
-     */
-    protected function redirectTo(Request $request): ?string
+     * Обработка случая, при котором пользователь не авторизован
+    */
+    protected function unauthenticated($request, array $guards): void
     {
-        return $request->expectsJson() ? null : route('login');
+        throw new AuthenticationException('Вы не авторизованы', $guards);
     }
+
 }
