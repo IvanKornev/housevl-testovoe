9bcfc95af144edc39f0e9b5efa51674a59de71bb
IvanKornev <brovkin.komar@yandex.ru>
2023-12-02 18:02:15 +0300

    feat: удаляю корзину при создании заказа, и (пока) все фича-тесты проходят

--- app/Modules/Order/Services/OrderService.php
+++ app/Modules/Order/Services/OrderService.php
@@ -5,12 +5,21 @@ declare(strict_types=1);
 namespace App\Modules\Order\Services;
 
 use App\Modules\Order\Services\Contracts\IOrderService;
+use App\Modules\Order\Adapters\CartAdapter;
 use App\Modules\Order\DTO\CreateOrderDTO;
 
 final class OrderService implements IOrderService
 {
+    private CartAdapter $adapter;
+
+    public function __construct(CartAdapter $adapter)
+    {
+        $this->adapter = $adapter;
+    }
+
     public function create(CreateOrderDTO $data): string
     {
+        $this->adapter->delete($data->cartHash);
         return 'some-url.com';
     }
 }
